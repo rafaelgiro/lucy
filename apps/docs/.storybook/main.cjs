b1e8442222c671885ebf17e6fe5b357822ad15a1
Rafael Giro <me@rafaelgiro.dev>
2023-02-20 11:05:09 -0300

    initial commit

---
+++ apps/docs/.storybook/main.cjs
@@ -0,0 +1,34 @@
+const path = require("path");
+
+module.exports = {
+  stories: ["../**/*.stories.mdx", "../**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-vite",
+  },
+  features: {
+    storyStoreV7: true,
+  },
+  async viteFinal(config, { configType }) {
+    // customize the Vite config here
+    return {
+      ...config,
+      resolve: {
+        alias: [
+          {
+            find: "@acme/core",
+            replacement: path.resolve(
+              __dirname,
+              "../../../packages/acme-core/"
+            ),
+          },
+        ],
+      },
+    };
+  },
+};
